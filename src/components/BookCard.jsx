1da308ea6b14ce9a98e716465b5f69ca248241fe
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-07-28 19:17:33 +0700

    feat : Tambah fitur Tombol saku

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import defaultImg from "../images/defaultImg.jpeg";
-import { Card ,Grid, Typography } from "@mui/material";
+import { Button, Card ,Grid, Typography } from "@mui/material";
+
+
 
 function BookCard(props) {
+    const [favorites, setFavorite] = useState("");
+    useEffect(()=> async ()=>{
+        let savedFavorite = await localStorage.getItem('__Fav');
+        if(savedFavorite) {
+          setFavorite(savedFavorite);
+        }
+    },[])
   const { book } = props;
   let imgDefined;
   if (typeof book.volumeInfo.imageLinks.smallThumbnail === "undefined") {
@@ -10,6 +19,7 @@ function BookCard(props) {
   } else {
     imgDefined = true;
   }
+  
   return (
     <Grid sx={{ alignItems:`center`, justify:`center` }}>
       <Card variant="outlined">
@@ -22,6 +32,7 @@ function BookCard(props) {
         <Typography variant="h6">Judul : {book.volumeInfo.title}</Typography>
         <Typography variant="subtitle1">Penulis : {book.volumeInfo.authors}</Typography>
         <Typography variant="subtitle1">Tanggal terbit : {book.volumeInfo.publishedDate}</Typography>
+        <Button variant="contained" color="primary" type="submit" onClicked={favorites}>Tambah ke Saku</Button>
       </Card>
     </Grid>
   );
